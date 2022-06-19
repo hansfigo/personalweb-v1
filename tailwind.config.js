7f8c51bc65995c95c0f891eb31f76b17ec4ddb19
Hans Figo <hambasahaya0303@gmail.com>
2022-06-20 02:38:51 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container:{
+      center: true,
+      padding : '16px',
+    },
+    extend: {
+      colors : {
+        primary :'#FF0066',
+        grey :'#334155'
+      }, 
+      scale: {
+        '200': '2',
+      },
+      screens:{
+        '2xl' : '1320px'
+      }
+    },
+  },
+  plugins: [],
+}
